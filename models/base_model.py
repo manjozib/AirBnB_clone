91e156455eaaa35a3e296bbe9e78c296ba61d7f2
DeazStar <deadfromtheborn@gmail.com>
2022-11-21 20:45:59 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Define a Basemodel class """
 import uuid
 import datetime
+import models
 
 
 class BaseModel:
@@ -28,10 +29,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """update the current time of updated_at attribute"""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """dictionary containing all keys/values of __dict__ of the instance:
