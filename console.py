fae5f9e24c1b53ae6210fdddfc430d520b26b9d4
DeazStar <deadfromtheborn@gmail.com>
2022-11-22 11:41:01 -0700

    add destroy and all functions

--- console.py
+++ console.py
@@ -39,7 +39,6 @@ class HBNBCommand(cmd.Cmd):
         based on the class name and id"""
         classes = models.storage.return_class()
         lines = line.split(' ')
-        instant_list = {}
         if len(line) == 0:
             print("** class name missing **")
         elif classes.get(lines[0]) is None:
@@ -51,6 +50,44 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(models.storage.all()[lines[0] + "." + lines[1]])
+
+    def do_destroy(self, line):
+        """ Deletes an instance based on the class name and id"""
+        classes = models.storage.return_class()
+        lines = line.split(' ')
+        if len(line) == 0:
+            print("** class name missing **")
+        elif classes.get(lines[0]) is None:
+            print("** class doesn't exist **")
+        elif len(lines) == 1:
+            print("** instance id missing **")
+        else:
+            if not models.storage.all().get(lines[0] + "." + lines[1]):
+                print("** no instance found **")
+            else:
+                del models.storage.all()[lines[0] + "." + lines[1]]
+                models.storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+        a = 0
+        new_list = []
+        if len(line) == 0:
+            print([str(v) for k, v in models.storage.all().items()])
+        else:
+            lines = line.split(' ')
+            for k, v in models.storage.all().items():
+                key = k.split('.')
+                if key[0] == line:
+                    a += 1
+                    new_list.append(str(v))
+                else:
+                    continue
+            if a == 0:
+                print("** class doesn't exist **")
+            else:
+                print(new_list)
 
 
 if __name__ == '__main__':
