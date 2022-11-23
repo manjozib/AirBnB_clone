7264293742b40dd08afc81708341f3b6e8dec3d1
DeazStar <deadfromtheborn@gmail.com>
2022-11-23 09:25:08 -0700

    chage folder and fix pycodestyle

---
+++ tests/test_models/test_save_reload_base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+import models
+from models.engine.file_storage import FileStorage
+import os.path
+
+
+class TestFileStorgar(unittest.TestCase):
+    def test_all_notempty(self):
+        """test if the function all works and new works"""
+        new_model = BaseModel()
+        models.storage.new(new_model)
+        message = "the dictinary is not empty"
+        self.assertIsNotNone(models.storage.all(), message)
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_save(self):
+        """test for the save file """
+        if os.path.exists("file.json"):
+            pass
+        else:
+            new_model = BaseModel()
+            models.storage.save()
+            file_exist = os.path.exists("file.json")
+            self.assertTrue(file_exist)
+
+    def test_relode(self):
+        """ test the relode method"""
+        new_model = BaseModel()
+        models.storage.reload()
+        new_next_model = models.storage.all()
+        message = "instance recreated"
+        self.assertIsNotNone(new_next_model, message)
