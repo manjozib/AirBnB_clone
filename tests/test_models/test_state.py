35e4c02f0507cf17678b52f5c10bfbad35d3a3f1
DeazStar <deadfromtheborn@gmail.com>
2022-11-23 09:44:08 -0700

    test_state.py

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    def test_issubclass(self):
+        """check if User class is sub class of BaseModel"""
+        self.assertTrue(issubclass(State, BaseModel))
+
+    def test_instantiation(self):
+        """check if the object is instnace of the State class"""
+        new_state = State()
+        self.assertIsInstance(new_state, State)
+    
+    def test_attribute(self):
+        new_state = State()
+        self.assertTrue(hasattr(State, "name"))
