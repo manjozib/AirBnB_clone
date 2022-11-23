bb60be20e60b5db48027c517f0927a6bde08f8fb
DeazStar <deadfromtheborn@gmail.com>
2022-11-23 10:05:49 -0700

    fix

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -7,14 +7,15 @@ from models.base_model import BaseModel
 
 class TestState(unittest.TestCase):
     def test_issubclass(self):
-        """check if User class is sub class of BaseModel"""
+        """check if Amenity class is sub class of BaseModel"""
         self.assertTrue(issubclass(Amenity, BaseModel))
 
     def test_instantiation(self):
-        """check if the object is instnace of the State class"""
+        """check if the object is instnace of the Aminty class"""
         new_amenity = Amenity()
         self.assertIsInstance(new_amenity, Amenity)
     
     def test_attribute(self):
+        """check if the attributes exist"""
         new_aminty = Amenity()
         self.assertTrue(hasattr(Amenity, "name"))
